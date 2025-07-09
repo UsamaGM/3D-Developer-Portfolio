b8ed9ee9939e641a7cf1b8cb4bec184ec0bfbfc1
UsamaGM <usamapk7861@gmail.com>
2025-07-10 00:04:47 +0500

    feat: Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import NavBar from "./components/NavBar";
 import "./index.css";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -5,6 +6,7 @@ import ShowcaseSection from "./sections/ShowcaseSection";
 export function App() {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
     </>
